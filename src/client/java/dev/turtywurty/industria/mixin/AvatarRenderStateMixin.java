3fae5cf5db72fe8cd3cfb40d7cd9d89a6cf9b827
TurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2026-05-07 14:16:47 +0100

    Fix crash with avatar render state mixin

--- src/client/java/dev/turtywurty/industria/mixin/AvatarRenderStateMixin.java
+++ src/client/java/dev/turtywurty/industria/mixin/AvatarRenderStateMixin.java
@@ -1,14 +1,18 @@
 package dev.turtywurty.industria.mixin;
 
+import dev.turtywurty.industria.util.ICompositeTextureState;
 import dev.turtywurty.industria.util.IStomachDestroyStage;
 import net.minecraft.client.renderer.entity.state.AvatarRenderState;
+import net.minecraft.resources.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(AvatarRenderState.class)
-public class AvatarRenderStateMixin implements IStomachDestroyStage {
+public class AvatarRenderStateMixin implements IStomachDestroyStage, ICompositeTextureState {
     @Unique
     public int stomachDestroyStage = 0;
+    @Unique
+    public Identifier compositeTexture = null;
 
     @Override
     public int industria$getStomachDestroyStage() {
@@ -18,5 +22,15 @@ public class AvatarRenderStateMixin implements IStomachDestroyStage {
     @Override
     public void industria$setStomachDestroyStage(int stage) {
         this.stomachDestroyStage = stage;
+    }
+
+    @Override
+    public void industria$setCompositeTexture(Identifier texture) {
+        this.compositeTexture = texture;
+    }
+
+    @Override
+    public Identifier industria$getCompositeTexture() {
+        return this.compositeTexture;
     }
 }
